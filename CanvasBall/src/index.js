be7d7075f065fc4a2beb00dfe487d3e12077f7e7
wheneva <wheneva14@gmail.com>
2019-04-25 14:35:07 +0800

    save + gitIgnore

--- CanvasBall/src/index.js
+++ CanvasBall/src/index.js
@@ -7,6 +7,19 @@ class Ball {
         this.y = y;
         this.radius = radius;
         this.color = "black";
+        this.dx = (Math.random()*0.10)-0.05;
+        this.dy = (Math.random()*0.10)-0.05;
+    }
+
+    update(delta) {
+        if(this.x - this.radius < 0 || this.x + this.radius > window.innerWidth) {
+            this.dx *= -1;
+        }
+        if(this.y - this.radius < 0 || this.y + this.radius > window.innerHeight) {
+            this.dy *= -1;
+        }
+        this.x += this.dx*delta;
+        this.y += this.dy*delta;
     }
 
     draw(ctx) {
@@ -26,7 +39,18 @@ main();
 function main() {
 
     var ballsArray = [];
-    var numberOfBalls = 1;
+    var numberOfBalls = 10;
+
+    var lastRanTime;
+    const TIMESTEP = 1000/60;
+    var delta = 0;
+
+    var mouse = {
+        x : null,
+        y : null
+    }
+
+    var raf = null;
     var pixelRatio = window.devicePixelRatio || 1;
     var canvas = document.createElement('canvas');
     canvas.setAttribute("id", "canvas");
@@ -40,40 +64,96 @@ function main() {
     
     function init() {
         for(let i=0; i< numberOfBalls; i++) {
-            let radius = 300;
+            let radius = (Math.random() * 5)+5;
             let x = Math.floor(Math.random() * (window.innerWidth-radius-radius))+radius;
             let y = Math.floor(Math.random() * (window.innerHeight-radius-radius))+radius;
             ballsArray.push( new Ball( x , y , radius) );
         }
-        requestAnimationFrame(animate);
+        raf = requestAnimationFrame(animate);
     }
 
-    function animate() {
+    function animate(timestamp) {
+        if(lastRanTime == null) {
+            lastRanTime = timestamp
+        }
+        delta = delta + (timestamp - lastRanTime);
         ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        
         ctx.beginPath();
         ctx.moveTo(0, 0);
-        ctx.lineTo(canvas.width, 0);
-        ctx.lineTo(canvas.width, canvas.height);
-        ctx.lineTo(0, canvas.height);
+        ctx.lineTo(window.innerWidth, 0);
+        ctx.lineTo(window.innerWidth, window.innerHeight);
+        ctx.lineTo(0, window.innerHeight);
         ctx.closePath();
         ctx.strokeStyle = "brown";
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 2;
         ctx.stroke();
+
+        ctx.moveTo(mouse.x, 0);
+        ctx.lineTo(mouse.x, window.innerHeight);
+        ctx.moveTo(0, mouse.y);
+        ctx.lineTo(window.innerWidth, mouse.y);
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 1;
+        ctx.stroke();
+
+        while(delta >= TIMESTEP) {
+            ballsArray.forEach(function(element) {
+                element.update(TIMESTEP);
+            })
+            delta -= TIMESTEP;
+        }
 
         ballsArray.forEach(function(element) {
             element.draw(ctx);
         })
 
-        requestAnimationFrame(animate);
+        ctx.font = `${20}px times`;
+        ctx.fillStyle = "#333";
+        ctx.fillText(canvas.width, 20, 20);
+        ctx.fillText(canvas.height, 20, 40);
+        ctx.fillText(window.innerWidth, 20, 60);
+        ctx.fillText(window.innerHeight, 20, 80);
+        ctx.fillText(pixelRatio, 20, 100);
+        ctx.fillText(mouse.x, 20, 120);
+        ctx.fillText(mouse.y, 20, 140);
+
+
+
+        lastRanTime = timestamp;
+        raf = requestAnimationFrame(animate);
     }
 
 
 
 
+    // window.addEventListener("mousedown", function(event) {
+    //     cancelAnimationFrame(raf);
+    // })
+    // window.addEventListener("mouseup", function(event) {
+    //     raf = requestAnimationFrame(animate);
+    // })
 
+    window.addEventListener("mousemove", function(event){
+        mouse.x = event.x;
+        mouse.y = event.y;
+    })
+    window.addEventListener("mouseout", function(event){
+        mouse.x = null;
+        mouse.y = null;
+    })
+    window.addEventListener('touchmove', function(e) {
+    // Cache the client X/Y coordinates
+        mouse.x = e.touches[0].clientX;
+        mouse.y = e.touches[0].clientY;
+    }, false);
 
-
+    window.addEventListener('touchend', function(e) {
+    // Cache the client X/Y coordinates
+        mouse.x = undefined;
+        mouse.y = undefined;
+    
+    // clearInterval(spawnId);
+    }, false);
 
 
 }

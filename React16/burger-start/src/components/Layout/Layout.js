d90306626fe035ffb1346b91a5647b28256cc24c
wheneva <wheneva14@gmail.com>
2019-05-08 15:34:59 +0800

    side drawer done

--- React16/burger-start/src/components/Layout/Layout.js
+++ React16/burger-start/src/components/Layout/Layout.js
@@ -1,14 +1,39 @@
 import React from "react";
 import "./Layout.scss";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-    <React.Fragment>
-        <div>Toolbar, side, backdrop</div>
-        <main className="Content">
-            {props.children}
-        </main>
-    </React.Fragment>
+class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showSideDrawer: false
+        }
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( prevState => ({
+            showSideDrawer : !prevState.showSideDrawer,
+        }))
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <Toolbar 
+                    drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer}/>
+                <main className="Content">
+                    {this.props.children}
+                </main>
+            </React.Fragment>
+        )
+    }
+}
     
-)
+    
 
-export default layout;
+
+export default Layout;
